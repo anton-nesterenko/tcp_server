504d87514193f0c5045c577239c4243e57cbd518
neurofen <neurofen@gmail.com>
2011-02-08 22:59:38 +1100

    tcp server now receives any rpc call and executes

--- src/tcp_server.erl
+++ src/tcp_server.erl
@@ -25,7 +25,8 @@
 %%%---------------------------------------------------------------------------------------
 
 init([Port]) ->
-	{ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
+	_Send_incoming_TCP_data_directly_to_process_as_messages = {active, true},
+	{ok, LSock} = gen_tcp:listen(Port, [_Send_incoming_TCP_data_directly_to_process_as_messages]),
 	{ok, #state{port = Port, lsock = LSock}, 0}.
 
 handle_call(get_count, _From, State) ->
@@ -34,14 +35,21 @@ handle_call(get_count, _From, State) ->
 handle_cast(stop, State) ->
 	{stop, normal, State}.
 
-handle_info(_Info, State) ->
-	{noreply, State}.
+handle_info({tcp, Socket, RawData}, State) ->
+	do_rpc(Socket, RawData),
+	RequestCount = State#state.request_count,
+	{noreply, State#state{request_count = RequestCount + 1}};
+
+handle_info(timeout, #state{lsock = LSock} = State) ->
+	{ok, _Sock} = gen_tcp:accept(LSock),
+	{noreply, State}. 
 
 terminate(_Reason, State) ->
 	ok.
 
 code_change(_OldVersion, State, _Extra) ->
 	{ok, State}.
+
 
 %%%---------------------------------------------------------------------------------------
 %%% implementation of this modules API
@@ -75,6 +83,7 @@ start_link() ->
 get_count() ->
 	gen_server:call(?SERVER, get_count).
 
+
 %%----------------------------------------------------------------------------------------
 %% @doc Stops this server
 %%
@@ -85,3 +94,31 @@ get_count() ->
 stop() ->
 	gen_server:cast(?SERVER, stop).
 
+%%%---------------------------------------------------------------------------------------
+%%% Internal functions
+%%%---------------------------------------------------------------------------------------
+
+do_rpc(Socket, RawData) ->
+	try
+		{M, F, A} = split_out_mfa(RawData),
+		Result = apply(M, F, A),
+		gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
+	catch
+		_Class:Err ->
+			gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Err]))
+
+	end.
+
+split_out_mfa(RawData) ->
+	MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
+	{match, [M, F, A]} = 
+		re:run(MFA,
+		       "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$",
+		       [{capture, [1,2,3], list}, ungreedy]),
+		       {list_to_atom(M), list_to_atom(F), args_to_terms(A)}.
+
+
+args_to_terms(RawArgs) ->
+	{ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++ "]. ", 1),
+	{ok, Args} = erl_parse:parse_term(Toks),
+	Args.
